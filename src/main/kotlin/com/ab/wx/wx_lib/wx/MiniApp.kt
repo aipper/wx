869068074a40ba421a57dfefbdc9f3fad4ff68bf
ab <mitsui2003>
2022-05-18 22:12:52 +0800

    fix

--- src/main/kotlin/com/ab/wx/wx_lib/wx/MiniApp.kt
+++ src/main/kotlin/com/ab/wx/wx_lib/wx/MiniApp.kt
@@ -4,16 +4,8 @@ import com.ab.wx.wx_lib.config.WxConfigProperties
 import com.ab.wx.wx_lib.vo.miniapp.Code2SessionVo
 import org.springframework.web.client.RestTemplate
 
-class MiniApp private constructor(miniAppId: String, miniAppSec: String) {
-    //    private var miniAppId = ""
-//    private var miniAppSec = ""
-    private var restTemplate: RestTemplate
-
-    init {
-//        this.miniAppId = miniAppId
-//        this.miniAppSec = miniAppSec
-        this.restTemplate = RestTemplate()
-    }
+object MiniApp {
+    private var restTemplate: RestTemplate = RestTemplate()
 
     fun getCode2Session(code: String): Code2SessionVo? {
         val code2sessionUrl = """
