d060c3913ba8797c5d11b6ada4f32075c0abd2fd
aipper <aipper@qq.com>
2024-05-20 16:25:06 +0800

    fix

---
+++ src/main/kotlin/com/ab/wx/wx_lib/dto/WxUploadMaterialDto.kt
@@ -0,0 +1,12 @@
+package com.ab.wx.wx_lib.dto
+
+
+/**
+ * 素材上传
+ */
+
+data class WxUploadImageDto(
+    val upload_type:Int = 1,
+    val resp_type:Int = 0,
+
+)
---
+++ src/main/kotlin/com/ab/wx/wx_lib/vo/WxUploadMaterialVo.kt
@@ -0,0 +1,4 @@
+package com.ab.wx.wx_lib.vo
+
+class WxUploadMaterialVo {
+}
